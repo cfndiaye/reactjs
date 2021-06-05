7513a6e0fb972075460ca9f3233a85d30215f89f
Cheikh faye ndiaye <cfndiaye@gmail.com>
2021-06-05 13:54:34 +0000

    final sopping list

--- soppinglist/src/App.js
+++ soppinglist/src/App.js
@@ -11,45 +11,111 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([
+    { itemName: 'item 1', quantity: 1, isSelected: false },
+    { itemName: 'item 2', quantity: 1, isSelected: false },
+    { itemName: 'item 3', quantity: 1, isSelected: false },
+  ]);
+
+  const [inputvalue, setInputValue] = useState('');
+
+  const handleAddButtonClick = () => {
+    if (!inputvalue) {
+      //do nothing
+    } else {
+      const newItem = { itemName: inputvalue, quantity: 1, isSelected: false };
+      const newItems = [...items, newItem];
+      setItems(newItems);
+      setInputValue('');
+    }
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleIncrementClick = (index) => {
+    const newItems = [...items];
+    newItems[index].quantity++;
+    setItems(newItems);
+  };
+  const handleDecrementClick = (index) => {
+    const newItems = [...items];
+    if (newItems[index].quantity === 0) {
+    } else newItems[index].quantity--;
+    setItems(newItems);
+  };
+
+  const toggleComplete = (index) => {
+    const newItems = [...items];
+    newItems[index].isSelected = !newItems[index].isSelected;
+
+    setItems(newItems);
+  };
+
+  const getTotal = () => {
+    const total = items.reduce((total, item) => {
+      if (item.isSelected) {
+        return total;
+      } else return total + item.quantity;
+    }, 0);
+    setTotal(total);
+  };
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => getTotal());
   return (
     <div className="app-background">
       <div className="main-container">
         <div className="add-item-box">
-          <input className="add-item-input" placeholder="add new item..." />
-          <FontAwesomeIcon icon={faPlus} />
+          <input
+            value={inputvalue}
+            className="add-item-input"
+            onChange={handleChange}
+            placeholder="add new item..."
+          />
+          <FontAwesomeIcon icon={faPlus} onClick={handleAddButtonClick} />
         </div>
         <div className="item-list">
-          <div className="item-container">
-            {items.map((item) => {
-              <>
-                <div className="item-name">
-                  {false ? (
-                    <>
-                      <FontAwesomeIcon icon={faCheckCircle} />
-                      <span className="completed">item 1</span>
-                    </>
-                  ) : (
-                    <>
-                      <FontAwesomeIcon icon={faCircle} />
-                      <span>item 1</span>
-                    </>
-                  )}
-                </div>
-                <div className="quantity">
-                  <button>
-                    <FontAwesomeIcon icon={faChevronLeft} />
-                  </button>
-                  <span>1</span>
-                  <button>
-                    <FontAwesomeIcon icon={faChevronRight} />
-                  </button>
-                </div>
-              </>;
-            })}
-          </div>
+          {items.map((item, index) => (
+            <div key={index} className="item-container">
+              <div className="item-name">
+                {item.isSelected ? (
+                  <>
+                    <FontAwesomeIcon
+                      icon={faCheckCircle}
+                      onClick={() => {
+                        toggleComplete(index);
+                      }}
+                    />
+                    <span className="completed">{item.itemName}</span>
+                  </>
+                ) : (
+                  <>
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      onClick={() => {
+                        toggleComplete(index);
+                      }}
+                    />
+                    <span>{item.itemName}</span>
+                  </>
+                )}
+              </div>
+              <div className="quantity">
+                <button onClick={() => handleDecrementClick(index)}>
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+                <span>{item.quantity}</span>
+                <button onClick={() => handleIncrementClick(index)}>
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="total">Total: 6</div>
+        <div className="total">Total: {total}</div>
       </div>
     </div>
   );
